f1765101db5b96c7ca236d585d08efaeda280557
Marcus Pereira <marcus.work@hotmail.com>
2021-05-22 17:56:26 -0300

    Adding some tests to Tag model

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
+  it { is_expected.to be_mongoid_document }
 
+  context "Associations" do
+    it { is_expected.to have_and_belong_to_many(:quotes).with_foreign_key(:quote_ids) }
+  end
+
+  context "Validations" do
+    it { is_expected.to validate_presence_of(:tag_name) }
+
+    it { is_expected.to have_field(:tag_name).of_type(String) }
+
+    it 'should create an instance of Tag' do
+      tag = Tag.new
+
+      expect(tag).to be_instance_of(Tag)
+    end
+
+    it 'should not be blank attributes' do
+      tag = Tag.new
+
+      expect(tag.tag_name).to be_falsey
+    end
+
+    it "should be valid with valid attributes" do
+      tag = Tag.new(tag_name: 'World')
+
+      expect(tag.class).to eq(Tag)
+      expect(tag.tag_name).to eq('World')
+      expect(tag).to be_valid
+    end
+  end
 end
