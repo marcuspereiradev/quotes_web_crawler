e4fcfdda3416afc6192954e8b6d344415f1564b9
Marcus Pereira <marcus.work@hotmail.com>
2021-05-29 19:49:54 -0300

    Refactoring crawler service

--- app/services/crawler_service.rb
+++ app/services/crawler_service.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 require 'net/http'
 require 'nokogiri'
 
+# Service that create or find quotes
 class CrawlerService
   def self.execute(params_search_tag)
     fetch_quote(params_search_tag)
@@ -21,11 +24,15 @@ class CrawlerService
       hash['author_about'] = "#{uri}#{element.css('span')[1].css('a')[0]['href']}"
       hash['tags'] = quote_tags.map { |tag| Tag.find_or_create_by(tag_name: tag) }
 
-      quote = Quote.find_or_initialize_by(quote: hash['quote'],
-                                          author: hash['author'],
-                                          author_about: hash['author_about'])
-      quote.tags = hash['tags']
-      quote.save
+      find_or_create_quotes(hash)
     end
+  end
+
+  def self.find_or_create_quotes(hash)
+    quote = Quote.find_or_initialize_by(quote: hash['quote'],
+                                        author: hash['author'],
+                                        author_about: hash['author_about'])
+    quote.tags = hash['tags']
+    quote.save
   end
 end
