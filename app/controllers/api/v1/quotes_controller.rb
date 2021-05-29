edd4412c1a39abb70d71bd19a62e7799deef7d12
Marcus Pereira <marcus.work@hotmail.com>
2021-05-29 19:13:28 -0300

    Improving the code based in Rubocop

--- app/controllers/api/v1/quotes_controller.rb
+++ app/controllers/api/v1/quotes_controller.rb
@@ -1,50 +1,53 @@
-class Api::V1::QuotesController < Api::V1::ApiController
-  before_action :set_quote, only: %i[update destroy]
+# frozen_string_literal: true
 
-  def index
-    @quotes = Quote.all
-    render json: @quotes
-  end
+module Api
+  module V1
+    # Quotes controller defines the creation of quotes
+    class QuotesController < Api::V1::ApiController
+      before_action :set_quote, only: %i[update destroy]
 
-  def update
-    if @quote.nil?
-      head :not_found
-    elsif @quote.update(quote_params)
-      render json: @quote
-    else
-      head :unprocessable_entity
+      def index
+        @quotes = Quote.all
+        render json: @quotes
+      end
+
+      def update
+        return head :not_found if @quote.nil?
+
+        if @quote.update(quote_params)
+          render json: @quote
+        else
+          head :unprocessable_entity
+        end
+      end
+
+      def destroy
+        return head :not_found if @quote.nil?
+
+        @quote.destroy!
+        head :no_content
+      end
+
+      def find_quotes
+        CrawlerService.execute(params[:search_tag])
+
+        tag = Tag.with_tag_name(params[:search_tag]).first
+
+        return head :not_found if tag.nil?
+
+        @quotes = tag.quotes
+        render json: @quotes
+      end
+
+      private
+
+      def quote_params
+        params.require(:quote).permit(:quote, :author, :author_about)
+      end
+
+      def set_quote
+        @quote = Quote.where(_id: params[:id]).first
+      end
     end
-  end
-
-  def destroy
-    if @quote.nil?
-      head :not_found
-    else
-      @quote.destroy!
-      head :no_content
-    end
-  end
-
-  def find_quotes
-    CrawlerService.execute(params[:search_tag])
-
-    tag = Tag.with_tag_name(params[:search_tag]).first
-
-    if tag.nil?
-      head :not_found
-    else
-      @quotes = tag.quotes
-      render json: @quotes
-    end
-  end
-
-  private
-
-  def quote_params
-    params.require(:quote).permit(:quote, :author, :author_about)
-  end
-
-  def set_quote
-    @quote = Quote.where(_id: params[:id]).first
   end
 end
